20f339378da3ddd7d4dc6f284f60c92d7968c5e3
Joris Harnetiaux <jorisharnetiaux@gmail.com>
2024-09-24 15:46:51 +0200

    Updates

--- src/components/Layout/style.ts
+++
@@ -1,9 +0,0 @@
-const style = {
-  layout: {
-    maxWidth: 1240,
-    margin: '0 auto',
-    padding: 3,
-  },
-}
-
-export default style
--- src/hooks/index.tsx
+++
@@ -1,6 +0,0 @@
-export const useLocale = () => {
-  const { searchParams } = new URL(document.location.toString())
-  const locale = searchParams.get('locale') || 'en'
-
-  return locale
-}
